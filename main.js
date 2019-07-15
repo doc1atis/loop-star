f06040b8c14086dcac5f67e461b36c0d5a0100ac
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2019-07-15 11:40:07 -0400

    last part

--- main.js
+++ main.js
@@ -1,43 +1,39 @@
-const submitButton = document.querySelector('.submit');
+const submitButton = document.querySelector(".submit");
 submitButton.onclick = makeList;
 
 function makeList(event) {
   event.preventDefault();
   deletePreviousList();
 
-  const inputBox = document.querySelector('.user-input');
+  const inputBox = document.querySelector(".user-input");
   const num = inputBox.value;
-  let numy = Number(num)
-  while (numy >= 0) {
-    addNewLI(numy)
-    if (numy % 2 === 0) {
-      makeLatestLIBold()
+  const numy = Number(num);
+  let counter = 0;
+  while (counter < numy) {
+    counter++;
+    addNewLI(counter);
+    if (counter % 2 === 0) {
+      makeLatestLIBold();
     }
-    numy--
-
-      
-    }
-  5
-
+  }
 }
 
 function addNewLI(text) {
-  const ul = document.querySelector('.list');
-  const li = document.createElement('li');
+  const ul = document.querySelector(".list");
+  const li = document.createElement("li");
   li.innerText = text;
   ul.appendChild(li);
 }
 
 function deletePreviousList() {
-  const ul = document.querySelector('.list');
+  const ul = document.querySelector(".list");
   while (ul.hasChildNodes()) {
     ul.removeChild(ul.firstChild);
   }
-
 }
 
 function makeLatestLIBold() {
-  const ul = document.querySelector('.list');
+  const ul = document.querySelector(".list");
   const li = ul.lastChild;
-  li.style.fontWeight = 'bold';
+  li.style.fontWeight = "bold";
 }
